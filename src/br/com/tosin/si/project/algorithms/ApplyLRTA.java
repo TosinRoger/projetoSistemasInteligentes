60126172a5d7d455e0485b913e48a88815a1f0f5
Roger <roger.tosin@gmail.com>
2017-05-21 10:54:05 -0300

    logica de empate da escolha do caminho

--- src/br/com/tosin/si/project/algorithms/ApplyLRTA.java
+++ src/br/com/tosin/si/project/algorithms/ApplyLRTA.java
@@ -7,6 +7,7 @@ import br.com.tosin.si.project.utils.CONST;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by roger on 20/05/17.
@@ -42,6 +43,7 @@ public class ApplyLRTA {
 
         DIRECTIONS nextDirections = null;
         double best = COST_INFINITE;
+        Random random = new Random();
 
         if (alreadyVisited(currentPosition)) {
             cost[currentPosition.x][currentPosition.y] += COST_BASE;
@@ -55,52 +57,116 @@ public class ApplyLRTA {
         }
 
         // visinho sul
-        if ( currentPosition.y != (HEIGHT - 1) &&  (COST_BASE + cost[currentPosition.x][currentPosition.y + 1]) < best) {
-            best = (COST_BASE + cost[currentPosition.x][currentPosition.y + 1]);
-            visited.add(new Position(currentPosition.x, currentPosition.y + 1));
-            nextDirections = DIRECTIONS.S;
+        if (currentPosition.y != (HEIGHT - 1)) {
+            if ((COST_BASE + cost[currentPosition.x][currentPosition.y + 1]) < best){
+                best = (COST_BASE + cost[currentPosition.x][currentPosition.y + 1]);
+                visited.add(new Position(currentPosition.x, currentPosition.y + 1));
+                nextDirections = DIRECTIONS.S;
+            }
+            // se houver um empate escolhe randomicamente
+            else if ((COST_BASE + cost[currentPosition.x][currentPosition.y + 1]) == best) {
+                if (random.nextBoolean()) {
+                    best = (COST_BASE + cost[currentPosition.x][currentPosition.y + 1]);
+                    visited.add(new Position(currentPosition.x, currentPosition.y + 1));
+                    nextDirections = DIRECTIONS.S;
+                }
+            }
         }
 
         // visinho oeste
-        if ( currentPosition.x != 0 &&  (COST_BASE + cost[currentPosition.x - 1][currentPosition.y]) < best) {
-            best = (COST_BASE + cost[currentPosition.x - 1][currentPosition.y]);
-            visited.add(new Position(currentPosition.x - 1, currentPosition.y));
-            nextDirections = DIRECTIONS.O;
+        if ( currentPosition.x != 0) {
+            if((COST_BASE + cost[currentPosition.x - 1][currentPosition.y]) < best) {
+                best = (COST_BASE + cost[currentPosition.x - 1][currentPosition.y]);
+                visited.add(new Position(currentPosition.x - 1, currentPosition.y));
+                nextDirections = DIRECTIONS.O;
+            }
+            else if((COST_BASE + cost[currentPosition.x - 1][currentPosition.y]) == best) {
+                if (random.nextBoolean()) {
+                    best = (COST_BASE + cost[currentPosition.x - 1][currentPosition.y]);
+                    visited.add(new Position(currentPosition.x - 1, currentPosition.y));
+                    nextDirections = DIRECTIONS.O;
+                }
+            }
         }
 
         // visinho leste
-        if ( currentPosition.x != (WIDTH -1) &&  (COST_BASE + cost[currentPosition.x + 1][currentPosition.y]) < best) {
-            best = (COST_BASE + cost[currentPosition.x + 1][currentPosition.y]);
-            visited.add(new Position(currentPosition.x + 1, currentPosition.y));
-            nextDirections = DIRECTIONS.L;
+        if ( currentPosition.x != (WIDTH -1)) {
+            if ((COST_BASE + cost[currentPosition.x + 1][currentPosition.y]) < best) {
+                best = (COST_BASE + cost[currentPosition.x + 1][currentPosition.y]);
+                visited.add(new Position(currentPosition.x + 1, currentPosition.y));
+                nextDirections = DIRECTIONS.L;
+            }
+            else if ((COST_BASE + cost[currentPosition.x + 1][currentPosition.y]) == best) {
+                if (random.nextBoolean()) {
+                    best = (COST_BASE + cost[currentPosition.x + 1][currentPosition.y]);
+                    visited.add(new Position(currentPosition.x + 1, currentPosition.y));
+                    nextDirections = DIRECTIONS.L;
+                }
+            }
         }
 
         // visinho nordeste
-        if ((currentPosition.x != (WIDTH - 1) && currentPosition.y != 0) && (COST_BASE + cost[currentPosition.x + 1][currentPosition.y - 1]) < best) {
-            best = (COST_DIAGONAL + cost[currentPosition.x + 1][currentPosition.y - 1]);
-            visited.add(new Position(currentPosition.x + 1, currentPosition.y - 1));
-            nextDirections = DIRECTIONS.ND;
+        if ((currentPosition.x != (WIDTH - 1) && currentPosition.y != 0)){
+            if((COST_BASE + cost[currentPosition.x + 1][currentPosition.y - 1]) < best) {
+                best = (COST_DIAGONAL + cost[currentPosition.x + 1][currentPosition.y - 1]);
+                visited.add(new Position(currentPosition.x + 1, currentPosition.y - 1));
+                nextDirections = DIRECTIONS.ND;
+            }
+            else if ((COST_BASE + cost[currentPosition.x + 1][currentPosition.y - 1]) == best) {
+                if (random.nextBoolean()) {
+                    best = (COST_DIAGONAL + cost[currentPosition.x + 1][currentPosition.y - 1]);
+                    visited.add(new Position(currentPosition.x + 1, currentPosition.y - 1));
+                    nextDirections = DIRECTIONS.ND;
+                }
+            }
         }
 
         // visinho noroeste
-        if ((currentPosition.x != 0 && currentPosition.y != 0) && (COST_BASE + cost[currentPosition.x - 1][currentPosition.y - 1]) < best) {
-            best = (COST_DIAGONAL + cost[currentPosition.x - 1][currentPosition.y - 1]);
-            visited.add(new Position(currentPosition.x - 1, currentPosition.y - 1));
-            nextDirections = DIRECTIONS.NO;
+        if ((currentPosition.x != 0 && currentPosition.y != 0) ) {
+            if((COST_BASE + cost[currentPosition.x - 1][currentPosition.y - 1]) < best) {
+                best = (COST_DIAGONAL + cost[currentPosition.x - 1][currentPosition.y - 1]);
+                visited.add(new Position(currentPosition.x - 1, currentPosition.y - 1));
+                nextDirections = DIRECTIONS.NO;
+            }
+            else if ((COST_BASE + cost[currentPosition.x - 1][currentPosition.y - 1]) == best) {
+                if (random.nextBoolean()) {
+                    best = (COST_DIAGONAL + cost[currentPosition.x - 1][currentPosition.y - 1]);
+                    visited.add(new Position(currentPosition.x - 1, currentPosition.y - 1));
+                    nextDirections = DIRECTIONS.NO;
+                }
+            }
         }
 
         // visinho suldeste
-        if ((currentPosition.x != (WIDTH - 1) && currentPosition.y != (HEIGHT - 1)) && (COST_BASE + cost[currentPosition.x + 1][currentPosition.y + 1]) < best) {
-            best = COST_DIAGONAL + cost[currentPosition.x + 1][currentPosition.y + 1];
-            visited.add(new Position(currentPosition.x + 1, currentPosition.y + 1));
-            nextDirections = DIRECTIONS.SD;
+        if ((currentPosition.x != (WIDTH - 1) && currentPosition.y != (HEIGHT - 1))) {
+            if((COST_BASE + cost[currentPosition.x + 1][currentPosition.y + 1]) < best) {
+                best = COST_DIAGONAL + cost[currentPosition.x + 1][currentPosition.y + 1];
+                visited.add(new Position(currentPosition.x + 1, currentPosition.y + 1));
+                nextDirections = DIRECTIONS.SD;
+            }
+            else if((COST_BASE + cost[currentPosition.x + 1][currentPosition.y + 1]) == best) {
+                if (random.nextBoolean()) {
+                    best = COST_DIAGONAL + cost[currentPosition.x + 1][currentPosition.y + 1];
+                    visited.add(new Position(currentPosition.x + 1, currentPosition.y + 1));
+                    nextDirections = DIRECTIONS.SD;
+                }
+            }
         }
 
         // visinho suldoeste
-        if ((currentPosition.x != 0 && currentPosition.y != (HEIGHT - 1)) && (COST_BASE + cost[currentPosition.x - 1][currentPosition.y + 1]) < best) {
-            best = COST_DIAGONAL + cost[currentPosition.x - 1][currentPosition.y + 1];
-            visited.add(new Position(currentPosition.x - 1, currentPosition.y + 1));
-            nextDirections = DIRECTIONS.SD;
+        if ((currentPosition.x != 0 && currentPosition.y != (HEIGHT - 1))) {
+            if( (COST_BASE + cost[currentPosition.x - 1][currentPosition.y + 1]) < best) {
+//                best = COST_DIAGONAL + cost[currentPosition.x - 1][currentPosition.y + 1];
+                visited.add(new Position(currentPosition.x - 1, currentPosition.y + 1));
+                nextDirections = DIRECTIONS.SD;
+            }
+            else if( (COST_BASE + cost[currentPosition.x - 1][currentPosition.y + 1]) == best) {
+                if (random.nextBoolean()) {
+//                    best = COST_DIAGONAL + cost[currentPosition.x - 1][currentPosition.y + 1];
+                    visited.add(new Position(currentPosition.x - 1, currentPosition.y + 1));
+                    nextDirections = DIRECTIONS.SD;
+                }
+            }
         }
 
         return nextDirections;
